47d5ca695142c9e772f06306f1d236130b3cbabc
R4Waayush <12aayushtirole@gmail.com>
2024-12-28 15:24:18 +0530

    story feature added

--- task12(insta)/script.js
+++ task12(insta)/script.js
@@ -30,16 +30,22 @@ const users = [
         storyPhoto: "https://images.unsplash.com/photo-1695311510443-a616195bec72?q=80&w=3432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }
 ];
-
+let part1 = document.querySelector(".part1");
+let story = document.querySelector(".story");
 sum = ""
-part1 = document.querySelector(".part1")
-users.forEach(function(elem, idx){
-    sum += `<div class="story" }>
+users.forEach(function(elem,idx){
+    sum += `<div class="story">
                     <img id=${idx} src="${elem.profilePhoto}" alt="">
                     <h4>${elem.name}</h4>
                 </div>`
     
-});
+})
 
 part1.innerHTML = sum;
-
+overlay = document.querySelector(".overlay");
+part1.addEventListener("click", function(dets){
+    console.log(users[dets.target.id].storyPhoto);
+    overlay.style.display = "block";
+    overlay.style.backgroundImage = `url("${users[dets.target.id].storyPhoto})`;
+       
+})
